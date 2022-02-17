bd5e30bb2e533a0c58cfa76c8e4b19d2f6d2af04
Kain Nhantumbo <nhantumbok@gmail.com>
2022-02-17 17:21:36 +0200

    Otimizada a importação de SVG para o Header

--- src/Components/MainApp.jsx
+++ src/Components/MainApp.jsx
@@ -1,8 +1,37 @@
 import React from 'react';
 
+
 const MainApp = () => {
+
+    const notesData = [
+        {
+            id: 0,
+            content: 'Fazer a barba',
+            completed: false
+        },
+        {
+            id: 1,
+            content: 'Lavar a roupa',
+            completed: false
+        },
+        {
+            id: 2,
+            content: 'Ir ao Churrasco',
+            completed: true
+        },
+        {
+            id: 3,
+            content: 'Alimentar Coelhos',
+            completed: false
+        }
+    ]
+
     return (
-        <span>this.main</span>
+        <section className="mainContainer">
+            <div>
+                
+            </div>
+        </section>
     )
 }
 export default MainApp;
