ed27867212f74405d056338639ef3a9c2260516d
Kain Nhantumbo <nhantumbok@gmail.com>
2022-02-19 20:45:54 +0200

    Adicionado o botao Settings nop footer. Mais melhorias na UI.

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,17 @@
 import react from "react";
 import LargerButtons from "./LargerButtons";
+import { HiCog } from 'react-icons/hi';
 
 const Footer = () => {
+    const cogIcon = <HiCog/>;
     return (
         <footer>
-            <LargerButtons />
+            <div>
+                <LargerButtons icon={cogIcon}/>
+                <span>
+                    Settings
+                </span>
+            </div>
         </footer>
     );
 }
