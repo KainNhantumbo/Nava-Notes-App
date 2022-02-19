25a08dd950d07610741fcf9f690a3826c6a56cae
Kain Nhantumbo <nhantumbok@gmail.com>
2022-02-19 23:54:15 +0200

    Adicionado botões no Componente ModalConfirm

--- src/Components/ConfirmModal.jsx
+++ src/Components/ConfirmModal.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import HorizontalButtons from './HorizontalButtons';
+import { HiExclamation } from 'react-icons/hi';
 import { HiTrash } from 'react-icons/hi';
-
+import { HiX } from 'react-icons/hi';
 
 const ConfirmModal = () => {
+    const trashIcon =  <HiTrash/>
+    const xIcon = <HiX />
     return (
         <div className="modalContainer">
             <section>
                 <div className='icon'>
-                    <HiTrash/>
+                    <HiExclamation/>
                 </div>
                 <span>
                     Do you really want do delete this note?
                 </span>
                 <div className='actions'>
-                    <HorizontalButtons />
+                    <HorizontalButtons icon={trashIcon} description={'Confirm'}/>
+                    <HorizontalButtons icon={xIcon} description={'Cancel'}/>
                 </div>
             </section>
         </div>
