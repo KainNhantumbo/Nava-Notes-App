2ce4c2b7fcf399a2e9c3aec5a32548e69dcd7eaa
Kain Nhantumbo <nhantumbok@gmail.com>
2022-02-19 21:09:48 +0200

    Refatoração do Componente Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,16 +2,35 @@ import react from "react";
 import LargerButtons from "./LargerButtons";
 import { HiCog } from 'react-icons/hi';
 
-const Footer = () => {
+const footerButtons = () => {
     const cogIcon = <HiCog/>;
+
+    const data = [
+        {
+            icon: cogIcon,
+            description: 'Settings'
+        }
+    ];
+    return data;
+}
+
+const Footer = () => {
+    const data = footerButtons();
+    
     return (
         <footer>
-            <div>
-                <LargerButtons icon={cogIcon}/>
-                <span>
-                    Settings
-                </span>
-            </div>
+            {
+                data.map(value => {
+                    return (
+                        <div>
+                        <LargerButtons icon={value.icon}/>
+                            <span>
+                                {value.description}
+                            </span>
+                        </div>
+                    );
+                })
+            }
         </footer>
     );
 }
