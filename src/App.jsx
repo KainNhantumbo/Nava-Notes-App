432c8e331be2768cd4b1011679634795b40ea57d
Kain Nhantumbo <nhantumbok@gmail.com>
2022-02-27 10:32:09 +0200

    Reestruturação completa

--- src/App.jsx
+++ src/App.jsx
@@ -2,42 +2,21 @@ import Footer from './components/Footer';
 import { Route, Routes } from 'react-router-dom';
 import Archive from './pages/Archive';
 import Settings from './pages/Settings';
+import Home from './pages/Home';
 
-import Header from './components/Header';
-import { SearchBox } from './components/SearchBox';
-import NotesPackage from './components/NotesPackage';
-import ConfirmModal from './components/ConfirmModal';
-import { retrieveNotes } from './scripts/functions';
-import { ShowModal } from './scripts/functions';
+
 
 const App = () => {
-	const { removeModal, renderConfirmModal, visible } = ShowModal();
-	var notesData = retrieveNotes();
-	// notesData = [];
 	
-	console.log(notesData)
-	// controla o modal de confirmacao ao deletar nota
-
-
 	return (
 		<>
-			<Header title="Notes" 
-				child={<SearchBox />}
-			/>
-			<NotesPackage 
-				eventRemoveBtn={renderConfirmModal} 
-				notesData={notesData}
-			/>
-			<ConfirmModal 
-				removeModal={removeModal}
-				status={visible}
-			/>
-
-			<Footer />
 			<Routes>
+				<Route path='/' element={<Home />} />
 				<Route path='/pages/Archive' element={<Archive />} />
 				<Route path='/pages/Settings' element={<Settings />} />
 			</Routes>
+
+			<Footer />
 		</>
 	);
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,32 @@
+import Header from '../components/Header';
+import { SearchBox } from '../components/SearchBox';
+import NotesPackage from '../components/NotesPackage';
+import ConfirmModal from '../components/ConfirmModal';
+import { retrieveNotes } from '../scripts/functions';
+import { ShowModal } from '../scripts/functions';
 
 
 const Home = () => {
-  
+  const { removeModal, renderConfirmModal, visible } = ShowModal();
+  var notesData = retrieveNotes();
+  // notesData = [];
+
+  console.log(notesData)
+  // controla o modal de confirmacao ao deletar nota
 
   return (
     <>
-      
+      <Header title="Notes"
+        child={<SearchBox />}
+      />
+      <NotesPackage
+        eventRemoveBtn={renderConfirmModal}
+        notesData={notesData}
+      />
+      <ConfirmModal
+        removeModal={removeModal}
+        status={visible}
+      />
     </>
   );
 }
