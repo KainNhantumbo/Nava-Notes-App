af9a046ea49f5b0b3b0d1b3e216cecc992eab625
Kain Nhantumbo <nhantumbok@gmail.com>
2022-03-05 10:44:59 +0200

    Refatoração do componente Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,74 +11,24 @@ import { HiPencil } from 'react-icons/hi';
 import { HiPencilAlt } from 'react-icons/hi';
 import { AddNoteButton } from '../styles/styles';
 
-import { useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
-import { setDataToStorage } from '../scripts/core-functions';
 import { deleteNote } from '../scripts/core-functions';
+import { AddNoteInterfaceCore } from '../scripts/core-functions';
 
 const Home = () => {
   const pencilAlt = <HiPencilAlt />
-  const { removeModal, renderConfirmModal, visible } = ShowModal();
+  const { removeModal, removeNote, renderConfirmModal, visible } = ShowModal();
   var notesData = retrieveNotes();
 
-  const [interfaceStatus, setInterfaceStatus] = useState(false);
-  const [textValue, setTextValue] = useState('');
-  const [titleValue, setTitleValue] =  useState('');
-
-  // const AddNoteInterfaceCore = () => {
-    
-  // }
-  // renderiza a interface para adicionar nota
-  const renderAddNoteInterface = () => setInterfaceStatus(true);
+  const {
+    renderAddNoteInterface,
+    removeAddNoteInterface,
+    discardNote,
+    interfaceStatus,
+    getTextValue,
+    getTitleValue,
+    saveNote
+  } = AddNoteInterfaceCore();
   
-  // remove a interface para adicionar nota
-  const removeAddNoteInterface = () => {
-    resetValues();
-    setInterfaceStatus(false);
-  };
-
-  // remove a interface para adicionar nota
-  const discardNote = () => {
-    resetValues();
-    setInterfaceStatus(false);
-  };
-
-  // retorna o valor do state
-  const getTitleValue = e => setTitleValue(e.target.value);
-  const getTextValue = e => setTextValue(e.target.value);
-
-  // reseta os valores do state
-  const resetValues = () => {
-    setTextValue('');
-    setTitleValue('');
-  }
-  
-  // adiciona nota ao banco de dados
-  const saveNote = () => {
-    const id = uuidv4();
-    const date = new Date().toUTCString();
-
-    const newNote = {
-      id,
-      title: titleValue,
-      content: textValue,
-      createdAt: date
-    }
-
-    if (newNote.title === '' || newNote.content === '')
-    return;
-    
-    notesData.push(newNote)
-
-    // salva a nota no localstorage
-    setDataToStorage('notes', notesData)
-    removeAddNoteInterface()
-    return notesData;
-  }
-
-  // console.log(notesData[1].id);
-  
-
   return (
     <>
       {/* Header */}
@@ -94,7 +44,6 @@ const Home = () => {
       </AddNoteButton>
 
       {/* Main body */}
-      {/* renderConfirmModal fica ai no eventRemoveBtn */}
       <NotesPackage
         eventRemoveBtn={deleteNote} 
         notesData={notesData}
@@ -109,6 +58,7 @@ const Home = () => {
       />
       <ConfirmModal
         removeModal={removeModal}
+        removeNote={removeNote}
         status={visible}
       />
     </>
--- src/scripts/core-functions.js
+++ src/scripts/core-functions.js
@@ -1,18 +1,5 @@
 import { useState } from "react";
-
-//controla o comportamento do modal de confirmacao
-export function ShowModal() {
-  const [visible, setVisible] = useState(false)
-
-  const renderConfirmModal = () => setVisible(true);
-  const removeModal = () => setVisible(false);
-
-  return {
-    renderConfirmModal,
-    removeModal,
-    visible
-  }
-}
+import { v4 as uuidv4 } from 'uuid';
 
 // salva os dados no localStorage
 export const setDataToStorage = (key, data) => {
@@ -25,6 +12,24 @@ export const getDataFromStorage = (key) => {
   return data;
 }
 
+//controla o comportamento do modal de confirmacao
+export function ShowModal() {
+  const [visible, setVisible] = useState(false);
+
+  const renderConfirmModal = () => setVisible(true);
+  const removeModal = () => setVisible(false);
+  const removeNote = () => {
+    removeModal();
+  }
+
+  return {
+    renderConfirmModal,
+    removeModal,
+    removeNote,
+    visible
+  }
+}
+
 export const retrieveNotes = () => {
   var notesData = getDataFromStorage('notes');
 
@@ -32,7 +37,6 @@ export const retrieveNotes = () => {
     notesData = [];
     setDataToStorage('notes', notesData);
   }
-
   // sorteia os elementos pelo titulo
   return (
     notesData.sort((a, b) => {
@@ -53,4 +57,69 @@ export const deleteNote = (e) => {
     }
   }
   setDataToStorage('notes', notesData);
-} 
+}
+
+export const AddNoteInterfaceCore = () => {
+  const notesData = getDataFromStorage('notes');
+  const [interfaceStatus, setInterfaceStatus] = useState(false);
+  const [textValue, setTextValue] = useState('');
+  const [titleValue, setTitleValue] = useState('');
+
+  // renderiza a interface para adicionar nota
+  const renderAddNoteInterface = () => setInterfaceStatus(true);
+
+  // remove a interface para adicionar nota
+  const removeAddNoteInterface = () => {
+    resetValues();
+    setInterfaceStatus(false);
+  };
+
+  // remove a interface para adicionar nota
+  const discardNote = () => {
+    resetValues();
+    setInterfaceStatus(false);
+  };
+
+  // retorna o valor do state
+  const getTitleValue = e => setTitleValue(e.target.value);
+  const getTextValue = e => setTextValue(e.target.value);
+
+  // reseta os valores do state
+  const resetValues = () => {
+    setTextValue('');
+    setTitleValue('');
+  }
+
+  // adiciona nota ao banco de dados
+  const saveNote = () => {
+    const id = uuidv4();
+    const date = new Date().toUTCString();
+
+    const newNote = {
+      id,
+      title: titleValue,
+      content: textValue,
+      createdAt: date
+    }
+
+    if (newNote.title === '' || newNote.content === '')
+    return;
+    
+    notesData.push(newNote)
+
+    // salva a nota no localstorage
+    setDataToStorage('notes', notesData)
+    removeAddNoteInterface()
+    return notesData;
+  }
+
+  return {
+    renderAddNoteInterface,
+    removeAddNoteInterface,
+    discardNote,
+    interfaceStatus,
+    getTitleValue,
+    getTextValue,
+    saveNote
+  }
+}
