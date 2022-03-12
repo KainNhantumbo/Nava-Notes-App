71f5181e438cf0df3bedd85774ba4bcba388c27d
Kain Nhantumbo <nhantumbok@gmail.com>
2022-03-12 17:52:01 +0200

    Adicionada estilização ao componente About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,21 @@
 import { AboutContainer } from "../styles/aboutStyles.module";
 
 const About = () => {
-  return ( 
+  return (
     <AboutContainer>
-      
+      <div>
+        <section>
+          <span>Simple Notes App</span>
+        </section>
+        <section>
+        <span>App Version:</span> 0.0.7 (beta)
+        </section>
+        <section>
+        <span>Developer:</span> Kain Nhantumbo
+        </section>
+      </div>
     </AboutContainer>
   );
 }
- 
+
 export default About;
--- src/styles/aboutStyles.module.js
+++ src/styles/aboutStyles.module.js
@@ -5,36 +5,22 @@ import { colors } from './colors';
 export const AboutContainer = styled.section`
   @keyframes aboutAnimation {
     from {
-      border-left: 2px solid ${() => {
+      border: 2px solid ${() => {
         if (colors.primaryColor === '135, 86, 112') {
-          return `rgba(${colors.textColor}, .5)` 
-        } else {
-          return `rgb(${colors.secondaryColor})`
-        }
-      }};
-      border-right: 2px solid ${() => {
-        if (colors.primaryColor === '135, 86, 112') {
-          return `rgba(${colors.textColor}, .5)` 
+          return `rgb(${colors.primaryColor})`
         } else {
           return `rgb(${colors.secondaryColor})`
         }
       }};
       transition: .5s ease-in-out;
     }
-
+    
     to {
-      border-top: 2px solid ${() => {
+      border: 2px solid ${() => {
         if (colors.primaryColor === '135, 86, 112') {
-          return `rgba(${colors.textColor}, .5)` 
+          return `rgba(${colors.textColor}, .8)`
         } else {
-          return `rgb(${colors.secondaryColor})`
-        }
-      }};
-      border-bottom: 2px solid ${() => {
-        if (colors.primaryColor === '135, 86, 112') {
-          return `rgba(${colors.textColor}, .5)` 
-        } else {
-          return `rgb(${colors.secondaryColor})`
+          return `rgb(${colors.primaryColor})`
         }
       }};
       transition: .5s ease-in-out;
@@ -42,32 +28,61 @@ export const AboutContainer = styled.section`
   }
 
   display: flex;
-  justify-content: space-between;
-  flex-direction: row;
-  width: 90%;
-  max-width: 400px;
-  margin: .5em 0;
-  position: relative;
-  padding: 10px 8px;
-  line-height: 2em;
-  border-radius: 12px;
-  user-select: none;
-  box-shadow: 0 0 5px ${() => {
-    if (colors.primaryColor === '135, 86, 112') {
-      return `rgba(${colors.fullDarkColor}, .6)` 
-    } else {
-      return `rgba(${colors.primaryColor}, .6)`
-    }
-  }};
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 
-  border: 2px solid transparent;
-  background: ${() => {
-    if (colors.primaryColor === '135, 86, 112') {
-      return `rgb(${colors.whiteColor})`
-    } else {
-      return `rgb(${colors.backgroundColor})`
-    }
-  }};
+  div {
+    width: 90%;
+    max-width: 400px;
+    margin: .5em 0;
+    position: relative;
+    padding: 10px 8px;
+    line-height: 2em;
+    border-radius: 12px;
+    color: rgba(${colors.textColor}, .8);
+    border: 2px solid transparent;
+    animation: aboutAnimation 2000ms ease-in-out forwards infinite;
+    user-select: none;
+    box-shadow: 0 0 5px ${() => {
+      if (colors.primaryColor === '135, 86, 112') {
+        return `rgba(${colors.fullDarkColor}, .6)`
+      } else {
+        return `rgba(${colors.primaryColor}, .6)`
+      }
+    }};
+    background: ${() => {
+      if (colors.primaryColor === '135, 86, 112') {
+        return `rgb(${colors.whiteColor})`
+      } else {
+        return `rgb(${colors.backgroundColor})`
+      }
+    }};
 
-  animation: aboutAnimation 2000ms ease-in-out forwards infinite;
+    section {
+      text-align: center;
+      font-weight: 600;
+      color: ${() => {
+        if (colors.primaryColor === '135, 86, 112') {
+          return `rgb(${colors.primaryColor})`
+        } else {
+          return `rgb(${colors.primaryColor})`
+        }
+      }};
+      line-height: 1.6rem;
+      span {
+        padding: 2px 8px;
+        border-radius: 12px;
+        color: ${() => {
+          if (colors.primaryColor === '135, 86, 112') {
+            return `rgba(${colors.textColor}, .5)`
+          } else {
+            return `rgb(${colors.secondaryColor})`
+          }
+        }};
+      }
+    }
+
+  }
+
 `;
